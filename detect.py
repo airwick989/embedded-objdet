3b39277e5b556119a2b3c9e4c33114358c401ccb
airwick989 <ridwanhossain989@gmail.com>
2023-11-17 19:30:12 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -9,9 +9,12 @@ vidcapture = cv2.VideoCapture('traffic.mov')
 classifierCar = cv2.CascadeClassifier('carsignatures.xml')
 cars_counted = 0
 VIDEO_LEN_FRAMES = 500  #Video is 17s at 30fps
+frame_proc_times = []
 
 for i in range(0, VIDEO_LEN_FRAMES):
     
+    proc_start_time = time.time()
+
     placeholder, frames = vidcapture.read()
     grayscale = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
 
@@ -19,6 +22,12 @@ for i in range(0, VIDEO_LEN_FRAMES):
     detected_cars = classifierCar.detectMultiScale(grayscale, 1.1, 1)
     cars_counted = len(detected_cars)
     print(f"Cars counted in current frame: {cars_counted}")
+    curr_frame_proc_time = time.time() - proc_start_time
+    print(f"Time taken to process current frame: {curr_frame_proc_time} seconds\n")
+    frame_proc_times.append(curr_frame_proc_time)
 
 cv2.destroyAllWindows()
 print(f"OBJECT DETECTION EXECUTION TIME: {time.time() - start_time} seconds")
+print(f"FASTEST FRAME PROCESSING TIME: {min(frame_proc_times)} seconds")
+print(f"SLOWEST FRAME PROCESSING TIME: {max(frame_proc_times)} seconds")
+print(f"AVERAGE FRAME PROCESSING TIME: {sum(frame_proc_times)/len(frame_proc_times)} seconds")
