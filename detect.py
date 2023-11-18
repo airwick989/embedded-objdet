5bcdbc4502434ffcd00efedf4831e34c55d86812
airwick989 <ridwanhossain989@gmail.com>
2023-11-17 19:19:59 -0500

    Added video file and updated detection script

--- detect.py
+++ detect.py
@@ -1,12 +1,11 @@
 import cv2
-import numpy as np
 import time
 
 
 
 start_time = time.time()
 
-vidcapture = cv2.VideoCapture('../traffic.mov')
+vidcapture = cv2.VideoCapture('traffic.mov')
 classifierCar = cv2.CascadeClassifier('carsignatures.xml')
 cars_counted = 0
 VIDEO_LEN_FRAMES = 500  #Video is 17s at 30fps
@@ -14,14 +13,12 @@ VIDEO_LEN_FRAMES = 500  #Video is 17s at 30fps
 for i in range(0, VIDEO_LEN_FRAMES):
     
     placeholder, frames = vidcapture.read()
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+    grayscale = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
 
     # Perform detection of cars on screen for the single frame
-    cars = classifierCar.detectMultiScale(gray, 1.1, 1)
-    cars_counted = len(cars)
+    detected_cars = classifierCar.detectMultiScale(grayscale, 1.1, 1)
+    cars_counted = len(detected_cars)
     print(f"Cars counted in current frame: {cars_counted}")
 
 cv2.destroyAllWindows()
-
-print(f"Cars counted: {cars_counted}")
 print(f"OBJECT DETECTION EXECUTION TIME: {time.time() - start_time} seconds")
